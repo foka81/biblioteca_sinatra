7b5fcb1c2af81a7e89c3e81b63421084824e6ee9
PatrickMendC <milhionniverso@gmail.com>
2018-09-11 23:42:18 -0400

    Adicionadas rotas de cadastro e edição

--- controllers/cadastros_controller.rb
+++ controllers/cadastros_controller.rb
@@ -1,7 +1,27 @@
-get '/cadastros' do
-  erb :'cadastros/index'
-end
+#Rotas Cliente
 
 get '/paineladm/lista-cliente' do
   erb :lista_cliente
+end
+
+get '/paineladm/cadastra-cliente' do
+  erb :cadastra_cliente
+end
+
+get '/paineladm/edita-cliente' do
+  erb :edita_cliente
+end
+
+#Rotas Livro
+
+get '/paineladm/lista-livro' do
+  erb :lista_livro
+end
+
+get '/paineladm/cadastra-livro' do
+  erb :cadastra_livro
+end
+
+get '/paineladm/edita-livro' do
+  erb :edita_livro
 end
